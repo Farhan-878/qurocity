27c9038e255f64b62a77198917efb1abd222f067
Farhan-Dev <farhanmanzer878@gmail.com>
2024-09-17 11:49:11 +0530

    update the screen size show

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 767);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="App">
-      <Home />
+      {isSmallScreen ? (
+        <div className="coming-soon">
+          <div className="container">
+            <h1>
+              Coming Soon its under the development{" "}
+              <span>See in Big screen</span>
+            </h1>
+          </div>
+        </div>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
